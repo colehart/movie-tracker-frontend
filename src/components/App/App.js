3db7df02d88204272ffef5c5cf43bfe91f8a8840
Cole Hart <cole@colehart.com>
2018-10-24 14:59:06 -0600

    Add initial App structure in src/App/index.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import Route from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        WesTracker
+        <header>
+          <div className="logo">
+            <img
+              src="../assets/images/wes-logo.png"
+              className="logo-icon"
+              alt="An icon of Wes Anderson's face"
+            />
+            <h1>WesTracker</h1>
+          </div>
+          <Nav />
+        </header>
+        <Route />
       </div>
     );
   }
