7d1e2eb411f2bcd952f6ac430874d39d08443170
Cole Hart <cole@colehart.com>
2018-10-24 13:57:48 -0600

    Add ghost actions - guesswork structure

---
+++ src/actions/index.js
@@ -0,0 +1,23 @@
+// export const toggleFavorite = (id) => ({
+//   type: 'TOGGLE_FAVORITE',
+//   id
+// })
+
+// export const addUser = (email, password, id) => ({
+//   type: 'ADD_USER',
+//   email,
+//   password,
+//   id
+// })
+
+// export const loginUser = (email, password, id) => ({
+//   type: 'LOGIN_USER',
+//   email,
+//   password,
+//   id
+// })
+
+// export const logoutUser = (id) => ({
+//   type: 'LOGOUT_USER',
+//   id
+// })
