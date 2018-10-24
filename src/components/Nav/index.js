9a72fa72f1a84340140864d3127c06e4683e1ea5
Casey Hoepner <caseyhoepner@gmail.com>
2018-10-24 17:14:12 -0600

    Add Reset.css, add favorite and user icons, initially style header

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,20 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Nav.css';
 import { NavLink } from 'react-router-dom';
+import userIcon from '../../assets/images/margot.png'
 
 const Nav = () => {
   return (
-    <nav>
-      <NavLink to='/favorites'>
-        <div className='fav-btn'>
-          <span className='num-favs'>0</span>
+    <nav className="nav-container">
+      <NavLink 
+        className="nav-link" 
+        to="/favorites">
+        <div className="fav-btn">
+          <span className="num-favs">0</span>
         </div>
       </NavLink>
-      <NavLink to='/login' className='login-btn'>
+      <NavLink to="/login" className="login-btn">
           <img 
-            className='login-icon'
-            src='' />
-          <p className='login-text'></p>
+            className="login-icon"
+            src={ userIcon } />
+          <p className="login-text">Login</p>
       </NavLink>
     </nav>
   )
