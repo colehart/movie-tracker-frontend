9a7ddbede2140b7754d8227201476b9bc43d921c
Cole Hart <cole@colehart.com>
2018-10-25 14:27:15 -0600

    Write and pass App tests

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import * as API from '../../utils';
+import apiKey from '../../API-key.js';
 import { addMovies } from '../../actions';
+import MovieContainer from '../MovieContainer';
 import Nav from '../Nav';
 import wesIcon from '../../assets/images/wes.png';
 import './App.css';
-import * as API from '../../utils';
-import apiKey from '../../API-key.js';
-import MovieContainer from '../MovieContainer';
 
-class App extends Component {
+export class App extends Component {
   async componentDidMount() {
     const movies = await API.fetchMovies(apiKey)
     await this.props.addMovies(movies)
@@ -38,8 +40,12 @@ class App extends Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
+export const mapDispatchToProps = (dispatch) => ({
   addMovies: (movies) => dispatch(addMovies(movies))
 })
+
+App.propTypes = {
+  addMovies: PropTypes.func.isRequired
+}
 
 export default connect(null, mapDispatchToProps)(App);
