7e7abf33e085c88d8923bacbf964d6734146b5ae
Casey Hoepner <caseyhoepner@gmail.com>
2018-10-25 16:18:13 -0600

    Move all test files into test directory, write and pass moviesReducer tests

---
+++ src/tests/actions.test.js
@@ -0,0 +1,7 @@
+import * as Actions from '../actions'
+
+describe('actions', () => {
+  it('should have a type of ADD_MOVIES', () => {
+    // const movies = 
+  })
+})   
---
+++ src/tests/testMocks.js
@@ -0,0 +1,20 @@
+export const mockMovies = [
+  {
+    backdrop_path: "backdrop-path1.jpg",
+    id: 1,
+    overview: "Great film.",
+    poster_path: "poster-path1.jpg",
+    release_date: "2014-02-26",
+    title: "The Grand Budapest Hotel",
+    isFavorite: false
+  },
+  {
+    backdrop_path: "backdrop-path2.jpg",
+    id: 2,
+    overview: "Real good film.",
+    poster_path: "poster-path2.jpg",
+    release_date: "2015-03-27",
+    title: "The Life Aquatic with Steve Zissou",
+    isFavorite: false
+  }
+]
