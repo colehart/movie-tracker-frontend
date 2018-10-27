6f6111b58eae26fa1c57d4c763c1be858037474d
Cole Hart <cole@colehart.com>
2018-10-27 11:48:05 -0600

    Write and pass test for LoginForm email input"

--- src/containers/LoginForm/index.js
+++ src/containers/LoginForm/index.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
+import * as API from '../../utils'
 
 export class LoginForm extends Component {
   constructor() {
     super()
     this.state = {
-
+      email: ''
     }
   }
 
+  handleInputChange = (event) => {
+    const { name, value } = event.target
+    this.setState({[name]: value})
+  }
+
   render() {
+    const {email} = this.state
+
     return(
       <form>
-        <input />
+        <h2>Have an account?</h2>
+        <input
+          className="email-login"
+          name="email"
+          value={email}
+          onChange={this.handleInputChange}
+          placeholder="wes@anderson.com"
+        />
       </form>
     )
   }
--- src/tests/LoginForm.test.js
+++ src/tests/LoginForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import { LoginForm } from '../containers/LoginForm'
 
 describe('LoginForm', () => {
@@ -14,5 +14,17 @@ describe('LoginForm', () => {
 
   it('state should match the snapshot', () => {
     expect(wrapper.state()).toMatchSnapshot()
+  })
+
+  it('should update state with change to inputs', () => {
+    wrapper = mount(<LoginForm />)
+
+    const spy = spyOn(wrapper.instance(), 'handleInputChange')
+      wrapper.instance().forceUpdate()
+      const mockEvent = { target: { value: 'something'} }
+
+      wrapper.find('.email-login').simulate('change', mockEvent)
+
+      expect(spy).toHaveBeenCalled()
   })
 })
