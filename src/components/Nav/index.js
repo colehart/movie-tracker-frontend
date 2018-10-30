e45785d4e979feca32eb81e286a060fbe4268acc
Cole Hart <cole@colehart.com>
2018-10-30 14:12:25 -0600

    Get closer to solving transitions

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,63 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Nav.css';
 import { NavLink } from 'react-router-dom';
 import userIcon from '../../assets/images/margot.png'
 
-export class Nav extends Component {
-  constructor() {
-    super()
-    this.state = {
-      isActive: true,
+const Nav = (props) => {
+  const { userLoggedIn } = props
+
+  const checkActive = (event) => {
+    if(event.target.closest('a').classList.contains('active')) {
+
     }
   }
 
-  handleHover = (event) => {
-    debugger
-    if(event.target.classList.contains('active'))
-      this.setState({ isVisible: !this.state.isVisible })
-  }
-
-  render() {
-    const { userLoggedIn } = this.props
-    const { isActive } = this.state
-
-    return (
-      <nav className="nav-container">
-        <NavLink
-          className="fav-btn-group"
-          to="/favorites">
-          <div
-            className="fav-btn-nav"
-            onMouseOver={this.handleHover}
-            onMouseOut={this.handleHover}
-          >
-            <span className="num-favs">0</span>
-            <p
-              className={`fav-text ${isActive ? 'hidden' : 'slide'}`}
-            >
-              FAVORITES
-            </p>
-          </div>
-        </NavLink>
-        <NavLink to="/login"
-          className="login-btn"
-          onMouseOver={this.handleHover}
-          onMouseOut={this.handleHover}
-        >
-          <img
-            className="login-icon"
-            src={ userIcon }
-            alt='Click here to login'/>
+  return (
+    <nav className="nav-container">
+      <NavLink
+        to="/favorites"
+        className="fav-btn-group"
+      >
+        <div className="fav-btn-nav" >
+          <span className="num-favs">0</span>
           <p
-            className={`login-text ${isActive ? 'hidden' : 'slide'}`}
+            className={`favorites-text ${checkActive}`}
           >
-            {userLoggedIn ? 'LOGOUT' : 'LOGIN'}
+            FAVORITES
           </p>
-        </NavLink>
-      </nav>
-    )
-  }
+        </div>
+      </NavLink>
+      <NavLink
+        to="/login"
+        className="login-btn"
+      >
+        <img
+          className="login-icon"
+          src={ userIcon }
+          alt='Click here to login'/>
+        <p
+          className='login-text'
+        >
+          {userLoggedIn ? 'LOGOUT' : 'LOGIN'}
+        </p>
+      </NavLink>
+    </nav>
+  )
 }
 
 
