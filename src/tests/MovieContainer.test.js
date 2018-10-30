1645cbee9a3f32b733ece1b48906e2a052ceb14e
Cole Hart <cole@colehart.com>
2018-10-30 11:55:41 -0600

    Add mapStateToProps test for movies to Movie Container

--- src/tests/MovieContainer.test.js
+++ src/tests/MovieContainer.test.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { MovieContainer } from '../containers/MovieContainer';
+import { MovieContainer, mapStateToProps } from '../containers/MovieContainer';
 import { mockMovies } from './testMocks';
 
 describe('MovieContainer', () => {
-  let wrapper;
+  describe('MovieContainer Component', () => {
+    let wrapper;
 
-  beforeEach(() => {
-    wrapper = shallow(<MovieContainer movies={ mockMovies }/>);
+    beforeEach(() => {
+      wrapper = shallow(<MovieContainer movies={ mockMovies }/>);
+    })
+
+    it('should match the snapshot', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
   })
 
-  it('should match the snapshot', () => {
-    expect(wrapper).toMatchSnapshot()
+  describe('mapStateToProps', () => {
+    it('should parse the movies from state', () => {
+      const mockState = {
+        movies: mockMovies
+      }
+      const expected = mockMovies
+      const mappedProps = mapStateToProps(mockState)
+
+      expect(mappedProps.movies).toEqual(expected)
+    })
   })
 })
